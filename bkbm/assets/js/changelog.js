38612eeed17ac2223d8b53fc2b91c2926dcc5822
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-09-14 17:20:36 +0300

    cleanup files and folders

---
+++ bkbm/assets/js/changelog.js
@@ -0,0 +1,69 @@
+$(function () {
+  // Update the log text
+  function updatesLogTag(string) {
+    let Obj = {
+      "Updated:": "<b>Updated:</b>",
+      "Fixed:": "<b>Fixed:</b>",
+    }
+
+    return string.replace(/Updated:|Fixed:/gi, function (matched) {
+      return Obj[matched]
+    })
+  }
+
+  // Generate Dynamic Log File.
+  function generateDynamicLog(data) {
+    const $dynamic_log = $("#dynamic_log")
+    const commonLogText = "<b>Updated:</b> Plugin translation file & documentation."
+    if ($dynamic_log.length) {
+      data.forEach((element) => {
+        var outputHtml = "<div>"
+
+        outputHtml += `<p class="log_date">` + element[0] + `</p>`
+
+        if (element[1].length > 0) {
+          // console.log(element[1])
+
+          outputHtml += `<ul class="square-list">`
+
+          element[1].forEach((log) => {
+            outputHtml += `<li>` + updatesLogTag(log) + `</li>`
+          })
+
+          outputHtml += `<li>` + commonLogText + `</li>`
+          outputHtml += `</ul>`
+        }
+
+        outputHtml += "</div>"
+
+        $dynamic_log.before(outputHtml)
+      })
+    } else {
+      alert("#dynamic_log is missing. Example: srcare doc")
+    }
+  }
+
+  // Update Version and Last Update Info
+
+  function updateMetaInfo(metaData) {
+    const $lastUpdate = $(".update"),
+      $ver = $(".ver")
+
+    if ($lastUpdate.length) {
+      $ver.html("").html(metaData[0])
+      $lastUpdate.html("").html(metaData[1])
+    } else {
+      alert(".update is missing. Example: srcare doc")
+    }
+  }
+
+  var metaData = ["1.3.6", "09/09/2023"] // Change only the version and date
+  updateMetaInfo(metaData)
+
+  // Just add a new row.
+  var changeLogData = [
+    // Add a new row below this comment.
+    ["2023, September, 09 - v 1.3.6", ["Fixed: Plugin code issues.", "Fixed: Plugin translation issues.", "Updated: WooCommerce product voting addon to 1.1.1", "Updated: User Vote Tracker addon 1.0.7"]],
+  ]
+  generateDynamicLog(changeLogData)
+})
