6bc4871b1e31f070dd803cebcefe300c7fe8297d
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-10-27 09:50:29 +0300

    update the wpfm doc

--- wpfm/assets/js/changelog.js
+++ wpfm/assets/js/changelog.js
@@ -57,12 +57,13 @@ $(function () {
     }
   }
   // Change only the version and date
-  var metaData = ["1.3.9", "05/10/2023"]
+  var metaData = ["1.4.0", "27/10/2023"]
   updateMetaInfo(metaData)
 
   // Just add a new row.
   var changeLogData = [
     // Add a new row below this comment.
+    ["2023, October 27 - v 1.4.0", ["Fixed: Admin panel notice.", "Updated: Plugin for WooCommerce 8.2.1"]],
     ["2023, October 05 - v 1.3.9", ["Updated: Plugin translation file", "Updated: Plugin styles and javascripts."]],
     ["2023, September 15 - v 1.3.8", ["Fixed: Meta link issue for the options panel.", "Updated: Plugin translation file", "Updated: Plugin styles and javascripts."]],
     ["2023, September 03 - v 1.3.7", ["Fixed: Plugin code issues.", "Updated: Plugin for WooCommerce 8.3.0", "Updated: Plugin styles and javascripts."]],
