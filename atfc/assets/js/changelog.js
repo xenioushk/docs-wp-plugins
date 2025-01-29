4cb67eac19f582205d81f0b28f0c9f3f1b921f93
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-01-29 19:37:14 +0200

    Updated wpfm documentation.

--- atfc/assets/js/changelog.js
+++ atfc/assets/js/changelog.js
@@ -57,14 +57,14 @@ $(function () {
     }
   }
 
-  var metaData = ["1.3.1", "28/01/2025"] // Change only the version and date
+  var metaData = ["1.3.1", "29/01/2025"] // Change only the version and date
   updateMetaInfo(metaData)
 
   // Just add a new row.
 
   var changeLogData = [
     // Add a new row below this comment.
-    ["2025, January, 28 - v 1.3.1", ["Fixed: Attachment size display issue.", "Fixed: Options panel colorpicker display issue.", "Updated: Plugin for WooCommerce 9.6.0"]],
+    ["2025, January, 29 - v 1.3.1", ["Fixed: Attachment size display issue.", "Fixed: Options panel colorpicker display issue.", "Updated: Plugin for WooCommerce 9.6.0"]],
     ["2024, December, 08 - v 1.3.0", ["Updated: Plugin for WordPress 6.7.1", "Updated: Plugin for WooCommerce 9.4.3"]],
     ["2024, August 21 - v 1.2.9", ["Updated: Plugin for WooCommerce 9.3.2"]],
     ["2024, July 29 - v 1.2.8", ["Updated: Plugin for WooCommerce 9.1.4"]],
