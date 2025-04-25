743ef0570318d88621a901710d171699fafc4710
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-25 12:51:20 +0300

    ✅ Updated: ATFC 1.3.5 doc.

--- atfc/assets/js/changelog.js
+++ atfc/assets/js/changelog.js
@@ -59,13 +59,14 @@ $(function () {
     }
   }
 
-  var metaData = ["1.3.4", "29/03/2025"] // Change only the version and date
+  var metaData = ["1.3.5", "25/04/2025"] // Change only the version and date
   updateMetaInfo(metaData)
 
   // Just add a new row.
 
   var changeLogData = [
     // Add a new row below this comment.
+    ["2025, April, 25 - v 1.3.5", ["Fixed: Database namespace issue.", "Updated: Plugin for WordPress 6.8", "Updated: Plugin notices and meta info."]],
     ["2025, March, 29 - v 1.3.4", ["Fixed: Issue with options panel data loading in the frontend."]],
     ["2025, March, 27 - v 1.3.3", ["Fixed: Font-awesome icon loading issue."]],
     ["2025, March, 23 - v 1.3.2", ["Fixed: Attachment count issue.", "Fixed: Quick edit issue.", "Fixed: media file global attachment checkbox issue.", "Fixed: Attachment count and display issue.", "Updated: Plugin for WooCommerce 9.7.1"]],
